9bcdd585e60d9455e5ca9b71dad602b1c177427d
Dominic kasanga <dominickasanga@gmail.com>
2024-11-26 20:10:52 +0200

    modified:   db/migrate/20241123144403_add_facility_code_to_users.rb

--- db/migrate/20241123144403_add_facility_code_to_users.rb
+++ db/migrate/20241123144403_add_facility_code_to_users.rb
@@ -1,8 +1,9 @@
 class AddFacilityCodeToUsers < ActiveRecord::Migration[7.0]
   def change
+    # Add the column first
     add_column :users, :facility_code, :string, limit: 255, null: true, default: nil, if_not_exists: true
 
-    # Set the collation of the `facility_code` column to match `facilities.code`
+    # Explicitly adjust the column type, character set, and collation to match `facilities.code`
     execute <<-SQL.squish
       ALTER TABLE users
       MODIFY facility_code VARCHAR(255)
@@ -10,7 +11,10 @@ class AddFacilityCodeToUsers < ActiveRecord::Migration[7.0]
       COLLATE utf8mb4_0900_ai_ci
     SQL
 
+    # Add an index on the column
     add_index :users, :facility_code, if_not_exists: true
+
+    # Add the foreign key constraint
     add_foreign_key :users, :facilities, column: :facility_code, primary_key: :code, name: 'fk_users_facility_code', if_not_exists: true
   end
 end
