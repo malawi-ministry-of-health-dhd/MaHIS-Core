dc2ae7d2b275ca21a1ecc67e6931c655e08205f0
Dominic kasanga <dominickasanga@gmail.com>
2024-11-26 19:46:42 +0200

    modified:   db/migrate/20241123144403_add_facility_code_to_users.rb

--- db/migrate/20241123144403_add_facility_code_to_users.rb
+++ db/migrate/20241123144403_add_facility_code_to_users.rb
@@ -1,9 +1,7 @@
 class AddFacilityCodeToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :facility_code, :string, default: nil
-
-    add_foreign_key :users, :facilities, column: :facility_code,
-                                         primary_key: :code
+    add_column :users, :facility_code, :string, null: true, default: nil
+    add_index :users, :facility_code
+    add_foreign_key :users, :facilities, column: :facility_code, primary_key: :code, name: 'fk_users_facility_code'
   end
 end
-
