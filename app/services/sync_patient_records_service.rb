9a29195e3821f5cd9add21e1f54c13ebd777cb69
petroskayange <kayangepetros@gmail.com>
2024-12-05 14:02:19 +0200

    improving getting of ids to be sync query by using previous_sync_date

--- app/services/sync_patient_records_service.rb
+++ app/services/sync_patient_records_service.rb
@@ -4,20 +4,19 @@ module SyncPatientRecordsService
   class << self
     include ModelUtils
 
-    def get_not_sync_ids(ids)
+    def get_not_sync_ids(previous_sync_date = nil, enable_site_sync= nil)
       location_id = User.current.location_id
-      
       query = PatientIdentifier
               .joins(patient: :encounters)
-              .where(encounters: { location_id: location_id })
               .where('patient_identifier.identifier_type = ?', 3)
               .distinct
-
-      return query.pluck(:patient_id) if ids.blank?
-
-      query
-        .where.not(identifier: ids)
-        .pluck(:patient_id)
+      query= query.where(encounters: { location_id: location_id }) if enable_site_sync.present?
+      query = query.where('encounter.encounter_datetime BETWEEN ? AND ?', previous_sync_date, Time.now.strftime("%Y-%m-%d %H:%M:%S")) if previous_sync_date.present?
+      latest_encounter_datetime = query.maximum('encounter.encounter_datetime')
+      {
+        not_synced_ids: query.pluck(:patient_id),
+        latest_encounter_datetime: latest_encounter_datetime
+      }
     end
   end
 end
