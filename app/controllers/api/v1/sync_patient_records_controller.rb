350d31f0e872a8b40c1bfcd4e9dc8d06e94c94b8
petroskayange <kayangepetros@gmail.com>
2024-12-05 14:02:33 +0200

    adding previous_sync_date params

--- app/controllers/api/v1/sync_patient_records_controller.rb
+++ app/controllers/api/v1/sync_patient_records_controller.rb
@@ -2,8 +2,9 @@
 class Api::V1::SyncPatientRecordsController < ApplicationController
       
   def get_not_sync_ids
-    ids = params[:ids]
-    records = SyncPatientRecordsService.get_not_sync_ids(ids)
+    previous_sync_date = params[:previous_sync_date]
+    enable_site_sync = params[:enable_site_sync]
+    records = SyncPatientRecordsService.get_not_sync_ids(previous_sync_date, enable_site_sync)
     render json: records, status: :ok
    
   end
