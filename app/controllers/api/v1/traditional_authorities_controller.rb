4edb9d0c746e85694fb6c0f5f9c7cde47d5a8b74
petroskayange <kayangepetros@gmail.com>
2024-12-30 19:00:08 +0200

    deletion of TAs and its villages

--- app/controllers/api/v1/traditional_authorities_controller.rb
+++ app/controllers/api/v1/traditional_authorities_controller.rb
@@ -24,6 +24,19 @@ module Api
           render json: paginate(TraditionalAuthority.where(*inexact_filters).order(:name))
         end
       end
+
+      def destroy
+        trad_auth = TraditionalAuthority.find(params[:id])
+        
+        ActiveRecord::Base.transaction do
+          trad_auth.villages.destroy_all
+          trad_auth.destroy!
+        end
+      
+        render json: { message: "Traditional Authority and associated villages successfully deleted" }, status: :ok
+        rescue ActiveRecord::RecordNotDestroyed => e
+          render json: { error: e.message }, status: :unprocessable_entity
+      end
     end
   end
 end
