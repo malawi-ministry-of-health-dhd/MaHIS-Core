b25522081019d00cc284c055001361f333ca3b43
petroskayange <kayangepetros@gmail.com>
2025-01-02 11:02:47 +0200

    adding of multiple villages

--- app/controllers/api/v1/person_addresses_controller.rb
+++ app/controllers/api/v1/person_addresses_controller.rb
@@ -5,38 +5,48 @@ module Api
     class PersonAddressesController < ApplicationController
       def create
         address_type = params[:address_type]
+        ta_name = params[:ta_name]
         parent_location = params[:parent_location]
-        address = params[:addresses_name]
+        addresses = params[:addresses]
 
-        data = add_address address, address_type, parent_location
-        render json: { data: data }
+        if address_type == 'Village' && addresses.is_a?(Array)
+          ta_data = ta_name ? create_ta(ta_name, parent_location) : ""
+          ta_id = ta_name ? ta_data.id : parent_location
+          data = addresses.is_a?(Array) ? add_multiple_villages(addresses, ta_id) : create_village(addresses, ta_id)
+        end
+
+        if address_type == 'TA'
+          create_ta(name, parent_location)
+        end
+
+        render json: { village_data: data, ta_data: ta_data }
       end
 
       private
 
-      def add_address(name, address_type, parent_location)
-        case address_type
-        when 'TA'
-          record = TraditionalAuthority.create!(
-            name: name,
-            district_id: parent_location,
-            creator: User.current.id,
-            date_created: Time.current
-          )
-        when 'Village'
-          record = Village.create!(
-            name: name,
-            traditional_authority_id: parent_location,
-            creator: User.current.id,
-            date_created: Time.current
-          )
-        else
-          raise ArgumentError, "Invalid address_type: #{address_type}"
+      def add_multiple_villages(villages, ta_id)
+        villages.map do |village|
+          create_village(village, ta_id)
         end
-        
-        record
       end
-      
+
+      def create_ta(name, district_id)
+        TraditionalAuthority.create!(
+          name: name,
+          district_id: district_id,
+          creator: User.current.id,
+          date_created: Time.current
+        )
+      end
+
+      def create_village(name, ta_id)
+        Village.create!(
+          name: name,
+          traditional_authority_id: ta_id,
+          creator: User.current.id,
+          date_created: Time.current
+        )
+      end
     end
   end
 end
