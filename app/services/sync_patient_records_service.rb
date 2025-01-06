7a713ef2e06048c5c592e4c18c1ccedaac62acd3
petroskayange <kayangepetros@gmail.com>
2025-01-06 14:38:57 +0200

    improve sync of patients

--- app/services/sync_patient_records_service.rb
+++ app/services/sync_patient_records_service.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
-
 module SyncPatientRecordsService
   class << self
     include ModelUtils
-
-    def get_not_sync_ids(previous_sync_date = nil, enable_site_sync= nil)
+    def get_not_sync_ids(previous_sync_date = nil, enable_site_sync = nil)
       location_id = User.current.location_id
-      query = PatientIdentifier
-              .joins(patient: :encounters)
-              .where('patient_identifier.identifier_type = ?', 3)
-              .distinct
-      query= query.where('encounter.location_id = ?', location_id ) 
-      query = query.where('encounter.encounter_datetime BETWEEN ? AND ?', previous_sync_date, Time.now.strftime("%Y-%m-%d %H:%M:%S")) if previous_sync_date.present?
-      latest_encounter_datetime = query.maximum('encounter.encounter_datetime')
+      
+      query = Encounter.where('encounter.location_id = ?', location_id)
+      query = query.where('encounter.date_created BETWEEN ? AND ?', 
+                         previous_sync_date, 
+                         Time.now.strftime("%Y-%m-%d %H:%M:%S")) if previous_sync_date.present?
+      
+      latest_encounter_datetime = query.maximum('encounter.date_created')
+      
       {
-        not_synced_ids: query.pluck(:patient_id),
+        not_synced_ids: query.distinct.pluck(:patient_id),
         latest_encounter_datetime: latest_encounter_datetime
       }
     end
