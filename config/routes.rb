d3eb5edf6bdb38ec17e35531a838b9f181ef719b
petroskayange <kayangepetros@gmail.com>
2025-01-23 10:02:05 +0200

    route to save patient record

--- config/routes.rb
+++ config/routes.rb
@@ -80,6 +80,7 @@ Rails.application.routes.draw do
         get '/visits' => 'patients#visits'
         get '/visit' => 'patients#visit'
         get '/get_patient_record' => 'patients#get_patient_record'
+        post '/save_patient_record' => 'patients#save_patient_record'
         get('/appointments', to: redirect do |params, request|
           paginate_url "/api/v1/appointments?patient_id=#{params[:patient_id]}",
                        request.params
