d93e84f7c0b08c6d12b77e8b96f148e3d0aa760d
petroskayange <kayangepetros@gmail.com>
2025-04-08 10:38:27 +0200

    adding creating of NCD number

--- app/services/save_patient_record_service.rb
+++ app/services/save_patient_record_service.rb
@@ -50,6 +50,7 @@ class SavePatientRecordService
       void_vaccine
       void_lab_order
       save_medication_order
+      create_ncd_identifier
     ].each { |operation| send(operation, patient_id, record) }
     BuildPatientRecordService.build_patient_record(patient_id)
   end
@@ -111,6 +112,45 @@ class SavePatientRecordService
     )
   end
 
+  def create_ncd_identifier(patient_id, record)
+    return unless record[:NcdID].present?
+    if(record[:NcdID] == "-")
+      PatientIdentifierService.create(patient_id: patient_id,
+      identifier: find_next_available_ncd_number(record[:location_id]),
+      identifier_type: 31)
+    end
+  end
+  def find_next_available_ncd_number(location_id)
+      current_ncd_code = global_property("site_prefix_#{location_id}")&.property_value
+      raise 'Global property `site_prefix` not set' unless current_ncd_code
+
+      type = PatientIdentifierType.find_by_name('NCD Number')
+      current_ncd_number_identifiers = PatientIdentifier.where(identifier_type: type)
+
+      unless current_ncd_number_identifiers.nil?
+        assigned_ncd_ids = current_ncd_number_identifiers.collect do |identifier|
+          Regexp.last_match(1).to_i if identifier.identifier =~ /#{current_ncd_code}-NCD- *(\d+)/
+        end.compact
+      end
+
+      next_available_number = nil
+
+      if assigned_ncd_ids.empty?
+        next_available_number = 1
+      else
+        assigned_numbers = assigned_ncd_ids.sort
+
+        possible_number_range = global_property('ncd_number_range')&.property_value&.to_i || 100_000
+
+        possible_identifiers = Array.new(possible_number_range) { |i| (i + 1) }
+        next_available_number = (possible_identifiers - assigned_numbers).first
+      end   
+
+      "NCD-#{current_ncd_code}-#{next_available_number}"
+  end
+  def global_property(name)
+    GlobalProperty.find_by property: name
+  end
   def enroll_program(patient_id, record)
     if PatientProgram.where(program_id: record[:program_id], patient_id: patient_id)
                      .exists?
