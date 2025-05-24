a57d4017749041edd2821499f6b1a24f8dbad235
petroskayange <kayangepetros@gmail.com>
2025-05-24 17:12:35 +0200

    change encounters.date_created to encounter.date_created

--- app/jobs/batch_patient_sync_job.rb
+++ app/jobs/batch_patient_sync_job.rb
@@ -51,7 +51,7 @@ class BatchPatientSyncJob
     # Add date filter if provided
     if since_date.present?
       parsed_date = Time.zone.parse(since_date.to_s)
-      query = query.where('encounters.date_created >= ?', parsed_date)
+      query = query.where('encounter.date_created >= ?', parsed_date)
     end
 
     # Return array of patient IDs to avoid issues with complex joined queries
