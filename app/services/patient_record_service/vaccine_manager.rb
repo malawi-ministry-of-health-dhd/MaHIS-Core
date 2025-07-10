21dc1ec17c09e1b9d14c120738bd639c7685f5b8
petroskayange <kayangepetros@gmail.com>
2025-07-10 09:11:53 +0200

    ✨ feat(patient): implement patient record services
    - introduce services for managing patient outcomes, enrollments, identities, SMS, and vaccines
    - create `OutcomeSaver` to handle patient outcome records
    - create `PatientEnrollmentManager` to manage program enrollments
    - create `PatientIdentityManager` to handle person and patient creation and identifier management
    - create `SmsManager` to manage sending SMS notifications
    - create `VaccineManager` to handle vaccine administration and voiding

---
+++ app/services/patient_record_service/vaccine_manager.rb
@@ -0,0 +1,52 @@
+# app/services/patient_record_service/vaccine_manager.rb
+# frozen_string_literal: true
+
+module PatientRecordService
+  class VaccineManager < BaseSaver
+    def save_vaccines(patient_id, record)
+      orders = record.dig(:vaccineAdministration, :orders)
+      return unless orders&.any?
+
+      begin
+        ActiveRecord::Base.transaction do
+          orders.each do |order|
+            encounter_id = create_encounter(patient_id, 25, record)
+
+            obs = record.dig(:vaccineAdministration, :obs)&.find do |item|
+              item[:value_text] == order[:drug_name]
+            end
+
+            AdministerVaccineService.administer_vaccine(encounter_id, [order], record[:program_id], [obs],
+                                                        record[:provider_id], record[:location_id])
+          end
+
+          record[:vaccineAdministration][:obs] = []
+          record[:vaccineAdministration][:orders] = []
+        end
+      rescue StandardError => e
+        log_error("Failed to save vaccines", e)
+      end
+    end
+
+    def void_vaccine(_patient_id, record)
+      data = record.dig(:vaccineAdministration, :voided)
+      return unless data&.any?
+
+      begin
+        ActiveRecord::Base.transaction do
+          data.each do |item|
+            order = Order.find(item[:order_id])
+            order.void(item[:reason])
+            Observation.where(order_id: order.id).each { |obs| obs.void(item[:reason]) }
+          end
+          record[:vaccineAdministration][:voided] = []
+        rescue ActiveRecord::RecordNotFound => e
+          log_error("Order not found", e)
+          record[:vaccineAdministration][:voided] = []
+        end
+      rescue StandardError => e
+        log_error("Error voiding vaccine", e)
+      end
+    end
+  end
+end
