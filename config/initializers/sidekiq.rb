e73bcd93d96ddf9049b3b2b4dfe01268e98cd6c5
petroskayange <kayangepetros@gmail.com>
2025-10-12 04:10:55 +0200

    🐛 fix(sidekiq): add namespace to redis connection
    - add namespace to redis connection to avoid conflicts with other applications

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,11 +6,11 @@ require 'sidekiq-cron'
 Sidekiq.default_job_options = { 'retry' => true } # Retry indefinitely
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'redis://localhost:6379/0', namespace: 'emr_api' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'redis://localhost:6379/0', namespace: 'emr_api' }
 end
 
 schedule_file = "config/schedule.yml"
