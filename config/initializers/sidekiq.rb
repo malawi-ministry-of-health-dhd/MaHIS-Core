d60a9c3e256687d3ab307b0f231f45fcab3c33d3
petroskayange <kayangepetros@gmail.com>
2025-10-12 05:14:56 +0200

    🐛 fix(sidekiq): correct redis database number
    - configure sidekiq to use redis database 0 instead of 1
    - this aligns with the default redis configuration and prevents potential data isolation issues

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,11 +6,11 @@ require 'sidekiq-cron'
 Sidekiq.default_job_options = { 'retry' => true } # Retry indefinitely
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/1'}
+  config.redis = { url: 'redis://localhost:6379/0'}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/1'}
+  config.redis = { url: 'redis://localhost:6379/0'}
 end
 
 schedule_file = "config/schedule.yml"
