5729dc0ed090fbdcc8adab50c45a4adf2b4fb1e0
petroskayange <kayangepetros@gmail.com>
2025-10-18 07:11:31 +0200

    new gems

--- vendor/bundle/ruby/3.2.0/gems/mongoid-9.0.6/lib/mongoid/version.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-# rubocop:todo all
-
-module Mongoid
-  VERSION = "9.0.6"
-end
--- vendor/bundle/ruby/3.2.0/gems/mongoid-9.0.6/spec/support/models/company.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-# rubocop:todo all
-
-class Company
-  include Mongoid::Document
-
-  embeds_many :staffs
-end
