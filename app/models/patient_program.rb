11d84ddcbfeb8db7a625a2593f95209f9ef95151
brian@xps <brianmsyamboza@gmail.com>
2025-11-07 11:35:32 +0200

    scope models to a location

--- app/models/patient_program.rb
+++ app/models/patient_program.rb
@@ -5,7 +5,8 @@ class PatientProgram < VoidableRecord
   self.primary_key = 'patient_program_id'
 
   after_void :after_void
-
+  include Locatable
+  
   belongs_to :patient
   belongs_to :program
   belongs_to :location, optional: true
