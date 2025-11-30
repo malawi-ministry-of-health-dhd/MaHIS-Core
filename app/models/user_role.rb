4e27ff308807756f5539dc54c3e81bc9b11fa324
petroskayange <kayangepetros@gmail.com>
2025-11-30 11:31:58 +0200

    Revert "scope models to a location"
    This reverts commit 4e0d8c7e19e0ae85cef220688d736d39d013114a.

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -4,8 +4,6 @@ class UserRole < ApplicationRecord
   self.table_name = :user_role
   self.primary_keys = :role, :user_id
 
-  include Locatable
-
   belongs_to :user, foreign_key: :user_id
   belongs_to :role, foreign_key: :role
 end
