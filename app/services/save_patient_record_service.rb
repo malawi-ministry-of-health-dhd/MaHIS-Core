2250ca9cc7d82dbcf76be44a92335b21af636c5d
petroskayange <kayangepetros@gmail.com>
2025-11-30 08:44:01 +0200

    ✨ feat(patient): implement dispensation data saving
    - add functionality to save dispensation data for medication orders
    - create `save_dispensation_data` method in `MedicationOrderSaver`
    - create private methods for building permitted data and processing dispensations
    - update `SavePatientRecordService` to use `MedicationOrderSaver` for saving dispensation data

--- app/services/save_patient_record_service.rb
+++ app/services/save_patient_record_service.rb
@@ -139,7 +139,7 @@ class SavePatientRecordService
       create_ncd_identifier: managers[:identity_manager].create_ncd_identifier(patient_id, record),
       save_notes_and_pharmalogical_notes: managers[:observation_saver].save_notes_and_pharmalogical_notes(patient_id, record),
       save_allergies: managers[:observation_saver].save_allergies(patient_id, record),
-      save_dispensation_data: managers[:dispensation_saver].save_dispensation_data(patient_id, record),
+      save_dispensation_data: managers[:medication_order_saver].save_dispensation_data(patient_id, record),
       save_all_observations: managers[:observation_saver].save_all_observations(patient_id, record)
     }
   end
