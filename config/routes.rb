95d326adb2df3b7cd21250bb93395ed859639575
petroskayange <kayangepetros@gmail.com>
2025-12-18 06:32:00 +0200

    removing files that use mongo

--- config/routes.rb
+++ config/routes.rb
@@ -79,7 +79,6 @@ Rails.application.routes.draw do
       end
 
       resources :roles
-      resources :printer_configurations, only: [:index, :create, :destroy, :update]
 
       # Patients
       get '/get_patient_record' => 'patients#get_patient_record'
