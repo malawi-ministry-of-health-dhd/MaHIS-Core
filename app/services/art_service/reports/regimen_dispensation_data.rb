c589b41ace791df4b39612cdba060c6820832aa8
Hopson Gausi <hopgausi20@gmail.com>
2026-03-27 10:55:25 +0200

    Refactor: Enhance regimen dispensation report with location-specific temporary table and index naming

--- app/services/art_service/reports/regimen_dispensation_data.rb
+++ app/services/art_service/reports/regimen_dispensation_data.rb
@@ -5,6 +5,7 @@ module ArtService
     # this report is used to generate regimen data
     class RegimenDispensationData
       include CommonSqlQueryUtils
+      include ArtTempTablesNaming
       attr_reader :start_date, :end_date, :type
 
       def initialize(start_date:, end_date:, **kwargs)
@@ -35,16 +36,16 @@ module ArtService
 
       # rubocop:disable Metrics/AbcSize
       def drop_regimen_data
-        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_current_dispensation'
-        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_drug_dispensed'
-        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_current_regimen_names'
-        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_current_patient_regimen'
-        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_reg_outcome'
-        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_vl_results'
-        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_current_vl_results'
-        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_regimen_patient_weight'
-        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_regimen_data'
-        ActiveRecord::Base.connection.execute 'DROP TABLE IF EXISTS temp_patient_start_date'
+        execute_query "DROP TABLE IF EXISTS #{temp_current_dispensation}"
+        execute_query "DROP TABLE IF EXISTS #{temp_drug_dispensed}"
+        execute_query "DROP TABLE IF EXISTS #{temp_current_regimen_names}"
+        execute_query "DROP TABLE IF EXISTS #{temp_current_patient_regimen}"
+        execute_query "DROP TABLE IF EXISTS #{temp_reg_outcome}"
+        execute_query "DROP TABLE IF EXISTS #{temp_vl_results}"
+        execute_query "DROP TABLE IF EXISTS #{temp_current_vl_results}"
+        execute_query "DROP TABLE IF EXISTS #{temp_regimen_patient_weight}"
+        execute_query "DROP TABLE IF EXISTS #{temp_regimen_data}"
+        execute_query "DROP TABLE IF EXISTS #{temp_patient_start_date}"
       end
       # rubocop:enable Metrics/AbcSize
 
@@ -62,7 +63,7 @@ module ArtService
 
       def create_temp_current_dispensation
         ActiveRecord::Base.connection.execute <<~SQL
-          CREATE TABLE temp_current_dispensation
+          CREATE TABLE #{temp_current_dispensation}
           SELECT o.patient_id, MAX(o.start_date) AS start_date
           FROM orders o
           INNER JOIN encounter e ON e.encounter_id = o.encounter_id AND e.voided = 0
@@ -75,66 +76,66 @@ module ArtService
           AND o.order_type_id = #{@drug_order_type_id}
           GROUP BY o.patient_id
         SQL
-        ActiveRecord::Base.connection.execute 'create index current_disp on temp_current_dispensation (patient_id, start_date)'
+        ActiveRecord::Base.connection.execute "CREATE INDEX #{idx_current_disp} ON #{temp_current_dispensation} (patient_id, start_date)"
       end
 
       def create_temp_drug_dispensed
         ActiveRecord::Base.connection.execute <<~SQL
-          CREATE table temp_drug_dispensed
+          CREATE table #{temp_drug_dispensed}
           SELECT o.patient_id, od.drug_inventory_id, d.name, o.start_date, od.quantity
           FROM orders o
           INNER JOIN encounter e ON e.encounter_id = o.encounter_id AND e.voided = 0
             #{@location_id ? "AND e.location_id = #{@location_id}" : ''}
           #{dsd_query(dsd: @dsd, model: 'o') if @dsd}
-          INNER JOIN temp_current_dispensation tcd ON tcd.patient_id = o.patient_id AND tcd.start_date = o.start_date
+          INNER JOIN #{temp_current_dispensation} tcd ON tcd.patient_id = o.patient_id AND tcd.start_date = o.start_date
           INNER JOIN drug_order od ON od.order_id = o.order_id AND od.quantity > 0 AND od.drug_inventory_id IN (SELECT drug_id FROM arv_drug)
           INNER JOIN drug d ON d.drug_id = od.drug_inventory_id AND d.retired  = 0
           WHERE o.voided = 0
           AND o.order_type_id = #{@drug_order_type_id}
         SQL
-        ActiveRecord::Base.connection.execute 'create index drug_disp on temp_drug_dispensed (patient_id, start_date)'
+        ActiveRecord::Base.connection.execute "CREATE INDEX #{idx_drug_disp} ON #{temp_drug_dispensed} (patient_id, start_date)"
       end
 
       def create_temp_current_regimen
         ActiveRecord::Base.connection.execute <<~SQL
-          CREATE table temp_current_regimen_names
+          CREATE table #{temp_current_regimen_names}
           SELECT GROUP_CONCAT(drug.drug_id ORDER BY drug.drug_id ASC) AS drugs, regimen_name.name AS name
           FROM moh_regimen_combination AS combo
           INNER JOIN moh_regimen_combination_drug AS drug USING (regimen_combination_id)
           INNER JOIN moh_regimen_name AS regimen_name USING (regimen_name_id)
           GROUP BY combo.regimen_combination_id
         SQL
-        ActiveRecord::Base.connection.execute 'create index regimen_names on temp_current_regimen_names (drugs(50))'
+        ActiveRecord::Base.connection.execute "CREATE INDEX #{idx_regimen_names} ON #{temp_current_regimen_names} (drugs(50))"
       end
 
       def create_temp_current_patient_regimen
         ActiveRecord::Base.connection.execute <<~SQL
-          CREATE table temp_current_patient_regimen
+          CREATE table #{temp_current_patient_regimen}
           SELECT d.patient_id, name
-          FROM temp_current_regimen_names tcrn
+          FROM #{temp_current_regimen_names} tcrn
           INNER JOIN (
             SELECT patient_id, GROUP_CONCAT(drug_inventory_id ORDER BY drug_inventory_id ASC) AS drugs
-            FROM temp_drug_dispensed
+            FROM #{temp_drug_dispensed}
             GROUP BY patient_id
           ) d ON d.drugs = tcrn.drugs
           #{dsd_query(dsd: @dsd, model: 'd') if @dsd}
         SQL
-        ActiveRecord::Base.connection.execute 'create index patient_regimen on temp_current_patient_regimen (patient_id)'
+        ActiveRecord::Base.connection.execute "CREATE INDEX #{idx_patient_regimen} ON #{temp_current_patient_regimen} (patient_id)"
       end
 
       def create_temp_patient_outcome
         ActiveRecord::Base.connection.execute <<~SQL
-          CREATE TABLE temp_reg_outcome
+          CREATE TABLE #{temp_reg_outcome}
           SELECT temp_current_dispensation.patient_id, #{@type == 'pepfar' ? "pepfar_patient_outcome(temp_current_dispensation.patient_id, #{@end_date})" : "patient_outcome(temp_current_dispensation.patient_id, #{@end_date})"} outcome
-          FROM temp_current_dispensation
+          FROM #{temp_current_dispensation} AS temp_current_dispensation
           #{dsd_query(dsd: @dsd, model: 'temp_current_dispensation') if @dsd}
         SQL
-        ActiveRecord::Base.connection.execute 'create index reg_outcome on temp_reg_outcome (patient_id)'
+        ActiveRecord::Base.connection.execute "CREATE INDEX #{idx_reg_outcome} ON #{temp_reg_outcome} (patient_id)"
       end
 
       def create_temp_vl_result
         ActiveRecord::Base.connection.execute <<~SQL
-          CREATE table temp_vl_results
+          CREATE table #{temp_vl_results}
           SELECT
             lab_result_obs.obs_datetime AS result_date,
             CONCAT (COALESCE(measure.value_modifier, '='),' ',COALESCE(measure.value_numeric, measure.value_text, '')) AS result,
@@ -154,31 +155,31 @@ module ArtService
           ) AS measure_concept ON measure_concept.concept_id = measure.concept_id
           #{dsd_query(dsd: @dsd, model: 'orders') if @dsd}
           WHERE lab_result_obs.voided = 0
-          AND measure.person_id IN (SELECT patient_id FROM temp_reg_outcome WHERE outcome = 'On antiretrovirals')
+          AND measure.person_id IN (SELECT patient_id FROM #{temp_reg_outcome} WHERE outcome = 'On antiretrovirals')
           AND (measure.value_numeric IS NOT NULL || measure.value_text IS NOT NULL)
           AND lab_result_obs.obs_datetime < #{end_date} + INTERVAL 1 DAY
           ORDER BY lab_result_obs.obs_datetime DESC
         SQL
-        ActiveRecord::Base.connection.execute 'create index vl_result on temp_vl_results (patient_id, result_date)'
+        ActiveRecord::Base.connection.execute "CREATE INDEX #{idx_vl_result} ON #{temp_vl_results} (patient_id, result_date)"
       end
 
       def create_temp_current_vl_results
         ActiveRecord::Base.connection.execute <<~SQL
-          CREATE TABLE temp_current_vl_results
+          CREATE TABLE #{temp_current_vl_results}
           SELECT t.*
-          FROM temp_vl_results t
-          LEFT JOIN temp_vl_results td ON td.patient_id = t.patient_id AND td.result_date > t.result_date
+          FROM #{temp_vl_results} t
+          LEFT JOIN #{temp_vl_results} td ON td.patient_id = t.patient_id AND td.result_date > t.result_date
           #{dsd_query(dsd: @dsd, model: 't') if @dsd}
           WHERE td.patient_id IS NULL
         SQL
-        ActiveRecord::Base.connection.execute 'create index current_vl on temp_current_vl_results (patient_id)'
+        ActiveRecord::Base.connection.execute "CREATE INDEX #{idx_current_vl} ON #{temp_current_vl_results} (patient_id)"
       end
 
       def create_temp_regimen_patient_weight
         ActiveRecord::Base.connection.execute <<~SQL
-          CREATE TABLE temp_regimen_patient_weight
+          CREATE TABLE #{temp_regimen_patient_weight}
           SELECT tro.patient_id, o.value_numeric AS weight
-          FROM temp_reg_outcome tro
+          FROM #{temp_reg_outcome} tro
           INNER JOIN (
             SELECT person_id, MAX(obs_datetime) AS max_datetime
             FROM obs
@@ -189,12 +190,12 @@ module ArtService
           INNER JOIN obs o ON o.person_id = latest_obs.person_id AND o.concept_id = 5089 AND o.obs_datetime = latest_obs.max_datetime AND o.voided = 0 AND o.obs_datetime < #{end_date} + INTERVAL 1 DAY
           WHERE tro.outcome = 'On antiretrovirals'
         SQL
-        ActiveRecord::Base.connection.execute 'create index patient_weight on temp_regimen_patient_weight (patient_id)'
+        ActiveRecord::Base.connection.execute "CREATE INDEX #{idx_patient_weight} ON #{temp_regimen_patient_weight} (patient_id)"
       end
 
       def create_temp_patient_start_date
         ActiveRecord::Base.connection.execute <<~SQL
-          CREATE TABLE temp_patient_start_date
+          CREATE TABLE #{temp_patient_start_date}
           SELECT
             `p`.`patient_id` AS `patient_id`,
             cast(patient_date_enrolled(`p`.`patient_id`) as date) AS `date_enrolled`,
@@ -211,10 +212,10 @@ module ArtService
             AND (`p`.`program_id` = #{@hiv_program_id})
             AND (`s`.`state` = #{@on_arv_state_id}))
             AND (`s`.`start_date` < #{end_date} + INTERVAL 1 DAY)
-            AND p.patient_id IN (SELECT patient_id FROM temp_reg_outcome WHERE outcome = 'On antiretrovirals')
+            AND p.patient_id IN (SELECT patient_id FROM #{temp_reg_outcome} WHERE outcome = 'On antiretrovirals')
           GROUP BY `p`.`patient_id`;
         SQL
-        ActiveRecord::Base.connection.execute 'create index start_date on temp_patient_start_date (patient_id)'
+        ActiveRecord::Base.connection.execute "CREATE INDEX #{idx_start_date} ON #{temp_patient_start_date} (patient_id)"
       end
 
       def clients_alive_on_treatment
@@ -231,15 +232,15 @@ module ArtService
             tcvr.result,
             tcp.name regimen,
             trc.earliest_start_date
-          FROM temp_patient_start_date trc
-          INNER JOIN temp_current_dispensation tcd ON tcd.patient_id = trc.patient_id AND tcd.start_date BETWEEN #{@start_date} AND #{@end_date}
+          FROM #{temp_patient_start_date} trc
+          INNER JOIN #{temp_current_dispensation} tcd ON tcd.patient_id = trc.patient_id AND tcd.start_date BETWEEN #{@start_date} AND #{@end_date}
           #{dsd_query(dsd: @dsd, model: 'trc') if @dsd}
           INNER JOIN person p ON p.person_id = trc.patient_id AND p.voided = 0
           INNER JOIN person_name pn ON pn.person_id = p.person_id AND pn.voided = 0
           LEFT JOIN patient_identifier i ON i.patient_id = p.person_id AND i.identifier_type = #{@arv_number_identifier_type_id} AND i.voided = 0
-          LEFT JOIN temp_regimen_patient_weight tpw ON tpw.patient_id = trc.patient_id
-          LEFT JOIN temp_current_vl_results tcvr ON tcvr.patient_id = trc.patient_id
-          LEFT JOIN temp_current_patient_regimen tcp ON tcp.patient_id = trc.patient_id
+          LEFT JOIN #{temp_regimen_patient_weight} tpw ON tpw.patient_id = trc.patient_id
+          LEFT JOIN #{temp_current_vl_results} tcvr ON tcvr.patient_id = trc.patient_id
+          LEFT JOIN #{temp_current_patient_regimen} tcp ON tcp.patient_id = trc.patient_id
           GROUP BY trc.patient_id
         SQL
 
@@ -250,7 +251,7 @@ module ArtService
       def alive_clients
         clients = ActiveRecord::Base.connection.select_all <<~SQL
           SELECT trc.patient_id
-          FROM temp_reg_outcome trc
+          FROM #{temp_reg_outcome} trc
           INNER JOIN person p ON p.person_id = trc.patient_id AND p.voided = 0
           where trc.outcome = 'On antiretrovirals' AND LEFT(p.gender, 1) = 'F'
         SQL
@@ -296,13 +297,17 @@ module ArtService
         result = []
         data = ActiveRecord::Base.connection.select_all <<~SQL
           SELECT tdd.name, DATE(tdd.start_date) start_date, tdd.quantity
-          FROM temp_drug_dispensed tdd
+          FROM #{temp_drug_dispensed} tdd
           WHERE tdd.patient_id = #{patient_id}
         SQL
         data.each do |row|
           result << { medication: row['name'], start_date: row['start_date'], quantity: row['quantity'] }
         end
         result
+      end
+
+      def execute_query(query)
+        ActiveRecord::Base.connection.execute(query)
       end
     end
   end
