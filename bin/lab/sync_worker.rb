b8ac3bf498bb9e0595e0697db5ddb061740ac548
Hopson Gausi <hopgausi20@gmail.com>
2026-04-27 11:24:40 +0200

    fix: update sync_worker to use calculated start_date for LIMS data processing

--- bin/lab/sync_worker.rb
+++ bin/lab/sync_worker.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-##
-# Rails runner script that runs/manages processes that push/pull data to/from LIMS.
+minimum_date = Date.parse('2026-01-01')
+sixty_days_ago = Date.today - 60.days
+start_date = [sixty_days_ago, minimum_date].max.to_s
 
-Lab::Lims::Worker.start
+Lab::Lims::Worker.start(start_date: start_date)
