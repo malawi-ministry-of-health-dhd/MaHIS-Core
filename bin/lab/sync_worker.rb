91e30bd2de37cd74dd79346481f4b3eb5ed6d62b
Hopson Gausi <hopgausi20@gmail.com>
2026-04-27 11:28:49 +0200

    fix: update sync_worker to limit start date to 120 days for historical data processing

--- bin/lab/sync_worker.rb
+++ bin/lab/sync_worker.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 minimum_date = Date.parse('2026-01-01')
-sixty_days_ago = Date.today - 60.days
+# To avoid processing an excessive amount of historical data, we limit the start date to 120 days ago or the minimum date, whichever is later.
+# 120 days because that's approximately 4 months, which is a reasonable window for syncing vl results that have taken long to process in the lab and may not have been synced yet.
+sixty_days_ago = Date.today - 120.days
 start_date = [sixty_days_ago, minimum_date].max.to_s
 
 Lab::Lims::Worker.start(start_date: start_date)
