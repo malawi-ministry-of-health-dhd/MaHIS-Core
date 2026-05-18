c18092be7dd5253754e85fa7e4ba7d9499838991
Dominic Kasanga <dominickasanga@gmail.com>
2026-05-18 13:38:17 +0200

    modified:   config/initializers/webauthn.rb

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
-allowed_origins = ENV.fetch(
-  'WEBAUTHN_ALLOWED_ORIGINS',
-  'http://localhost:5173,http://127.0.0.1:5173'
-).split(',').map(&:strip).reject(&:blank?)
-
 WebAuthn.configure do |config|
-  config.allowed_origins = allowed_origins
+  config.allowed_origins = ENV['WEBAUTHN_ALLOWED_ORIGINS']
+    &.split(',')&.map(&:strip)&.reject(&:blank?) || ->(_origin) { true }
   config.rp_name = ENV.fetch('WEBAUTHN_RP_NAME', 'MAHIS')
-  config.rp_id = ENV['WEBAUTHN_RP_ID'] if ENV['WEBAUTHN_RP_ID'].present?
+  config.rp_id = ENV.fetch('WEBAUTHN_RP_ID', 'localhost')
   config.credential_options_timeout = 120_000
 end
