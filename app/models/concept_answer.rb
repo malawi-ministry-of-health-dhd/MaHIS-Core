61cf77a6fbbba37dcdab5bd954ad03d32eb6a903
Wisdom Changala <50754450+Joutler@users.noreply.github.com>
2026-06-09 17:24:55 +0200

    Revert "ADD WHO PEN AND PEN PLUS - COMPLICATIONS SCREENING"

--- app/models/concept_answer.rb
+++ app/models/concept_answer.rb
@@ -4,7 +4,7 @@ class ConceptAnswer < ApplicationRecord
   self.table_name = :concept_answer
   self.primary_key = :concept_answer_id
 
-  belongs_to :answer, class_name: 'Concept', foreign_key: 'answer_concept', optional: true
-  belongs_to :drug, class_name: 'Drug', foreign_key: 'answer_drug', optional: true
+  belongs_to :answer, class_name: 'Concept', foreign_key: 'answer_concept'
+  belongs_to :drug, class_name: 'Drug', foreign_key: 'answer_drug'
   belongs_to :concept, class_name: 'Concept', foreign_key: 'concept_id'
 end
