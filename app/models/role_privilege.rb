d2165d2ceb0c33ec9de5cc7d6a81ddb728df6ac9
Dominic Kasanga <dominickasanga@gmail.com>
2026-06-11 10:46:54 +0200

    modified:   app/models/role_privilege.rb modified:   app/models/user_property.rb modified:   app/models/user_role.rb

--- app/models/role_privilege.rb
+++ app/models/role_privilege.rb
@@ -4,8 +4,6 @@ class RolePrivilege < ApplicationRecord
   self.table_name = 'role_privilege'
   self.primary_key = %i[privilege role]
 
-  audited
-
   belongs_to :role, foreign_key: :role
   belongs_to :privilege, foreign_key: :privilege
 end
