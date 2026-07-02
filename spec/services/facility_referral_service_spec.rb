3b129a6fd2f275f548a6efa5ce29bf69667e0a15
Louis Nyirongo <louisnyirongo73@gmail.com>
2026-07-02 12:10:02 +0200

    Refactor base referral SQL to simplify query logic and improve performance

--- spec/services/facility_referral_service_spec.rb
+++ spec/services/facility_referral_service_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe FacilityReferralService do
   describe '#base_referral_sql' do
     subject(:sql) { described_class.new.send(:base_referral_sql, {}) }
 
-    it 'excludes referrals that already have an encounter at the referred facility' do
+    it 'excludes referrals followed by a later encounter at the referred facility' do
       expect(sql).to include('NOT EXISTS')
       expect(sql).to include('referred_facility_encounter.patient_id = e.patient_id')
       expect(sql).to include('referred_facility_encounter.encounter_datetime > e.encounter_datetime')
-      expect(sql).to include('referred_facility_encounter_obs.encounter_id = referred_facility_encounter.encounter_id')
-      expect(sql).to include('CAST(referred_facility_encounter_obs.location_id AS UNSIGNED) = COALESCE')
+      expect(sql).to include('CAST(referred_facility_encounter.location_id AS UNSIGNED) = COALESCE')
+      expect(sql).not_to include('referred_facility_encounter_obs')
     end
   end
 end
