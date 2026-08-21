29a3dee1284a31e401b0d6135674b2388d00fe15
Hopson Gausi <hopgausi20@gmail.com>
2026-08-21 10:05:20 +0200

    Fix results notifications alerts not creating and sync work timing configuration

--- bin/lab/sync_worker.rb
+++ bin/lab/sync_worker.rb
@@ -5,7 +5,7 @@ Test::Unit::AutoRunner.need_auto_run = false if defined?(Test::Unit::AutoRunner)
 minimum_date = Date.parse('2026-01-01')
 # To avoid processing an excessive amount of historical data, we limit the start date to 120 days ago or the minimum date, whichever is later.
 # 120 days because that's approximately 4 months, which is a reasonable window for syncing vl results that have taken long to process in the lab and may not have been synced yet.
-sixty_days_ago = Date.today - 1.days
+sixty_days_ago = Date.today - 120.days
 start_date = [sixty_days_ago, minimum_date].max.to_s
 
 def start_lims_sync_workers(start_date:)
