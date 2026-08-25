2e3bec8e0b00bc0b94027d0d62bdd99363a151fa
fchiyenda <fchiyenda>
2026-08-25 18:21:18 +0200

    Reduce repeated workflow report queries

--- app/services/art_service/incomplete_visits_evaluator.rb
+++ app/services/art_service/incomplete_visits_evaluator.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 module ArtService
+  class OptimizedHtnWorkflow < HtnWorkflow
+    private
+
+    def global_property(name, location_id = nil)
+      @global_properties ||= {}
+      key = [name, location_id || User.current.location_id]
+      @global_properties.fetch(key) do
+        @global_properties[key] = super
+      end
+    end
+  end
+
   class OptimizedWorkflowEngine < WorkflowEngine
     def self.activities(property_value)
       encounters = (property_value&.split(',') || []).filter_map do |activity|
@@ -25,19 +37,20 @@ module ArtService
     def initialize(patient:, date:, program:, encounter_types: nil, encounters: nil, activities: nil,
                    observations: nil, patient_states: nil, arv_ids: nil, registered_patient_ids: nil,
                    staged_patient_ids: nil, clinician_ids: nil, concepts: nil,
-                   fast_track_enabled: nil, htn_enabled: nil)
+                   fast_track_enabled: nil, htn_enabled: nil, htn_workflow: nil)
       @patient = patient
       @program = program
       @date = date
       @activities = activities || load_user_activities
-      @fast_track_enabled = fast_track_enabled
+      @fast_track_enabled = fast_track_enabled == true
       @htn_enabled = htn_enabled
+      @htn_workflow = htn_workflow
       @encounter_types = encounter_types || EncounterType.where(name: workflow_states).index_by { |type| type.name.upcase }
       @todays_encounters = encounters || Encounter.unscoped
                                              .where(patient_id: patient.patient_id,
                                                     program_id: program.program_id,
                                                     encounter_datetime: day_bounds.first..day_bounds.last)
-                                             .includes(:orders)
+                                               .includes(orders: :drug_order)
                                              .to_a
                                       @observations = observations || []
                                       @patient_states = patient_states || []
@@ -83,15 +96,13 @@ module ArtService
     end
 
     def fast_track_activated?
-      return @fast_track_enabled unless @fast_track_enabled.nil?
-
-      super
+      @fast_track_enabled
     end
 
     def htn_transform(encounter_type)
       return encounter_type unless @htn_enabled
 
-      htn_workflow.next_htn_encounter(@patient, encounter_type, @date)
+      (@htn_workflow ||= OptimizedHtnWorkflow.new).next_htn_encounter(@patient, encounter_type, @date)
     end
 
     def workflow_states
@@ -134,6 +145,52 @@ module ArtService
       observations_for_value_drug.any? { |observation| observation.obs_datetime < @date.to_date }
     end
 
+    def patient_should_get_treatment?
+      return false if referred_to_clinician? && !seen_by_clinician?
+
+      !observations_for('Prescribe drugs').any? do |observation|
+        observation.value_coded.to_i == concept_id('No') && observation.obs_datetime.between?(*day_bounds)
+      end
+    end
+
+    def patient_got_treatment?
+      treatment_type_id = encounter_type(TREATMENT)&.encounter_type_id
+      @todays_encounters.any? do |encounter|
+        encounter.encounter_type == treatment_type_id &&
+          encounter.orders.any? { |order| !order.voided? && order.quantity.to_f.positive? }
+      end
+    end
+
+    def dispensing_complete?
+      treatment_type_id = encounter_type(TREATMENT)&.encounter_type_id
+      prescription = @todays_encounters
+                     .select { |encounter| encounter.encounter_type == treatment_type_id }
+                     .max_by(&:encounter_datetime)
+      return false unless prescription
+
+      drug_orders = prescription.orders.filter_map(&:drug_order)
+      !drug_orders.empty? && drug_orders.all? { |drug_order| drug_order.amount_needed <= 0 }
+    end
+
+    def patient_does_not_have_height_and_weight?
+      return true if patient_has_no_weight_today?
+      return true if patient_has_no_height?
+
+      patient_has_no_height_today? && patient_is_a_minor?
+    end
+
+    def patient_has_no_weight_today?
+      !observations_for('Weight').any? { |observation| observation.obs_datetime.between?(*day_bounds) }
+    end
+
+    def patient_has_no_height?
+      !observations_for('Height (cm)').any? { |observation| observation.obs_datetime < day_bounds.last }
+    end
+
+    def patient_has_no_height_today?
+      !observations_for('Height (cm)').any? { |observation| observation.obs_datetime.between?(*day_bounds) }
+    end
+
     def patient_is_alive?
       !@patient_states.any? do |state|
         state.patient_program.patient_id == @patient.patient_id &&
@@ -145,8 +202,32 @@ module ArtService
       !@registered_patient_ids.include?(@patient.patient_id)
     end
 
+    def patient_checked_in?
+      reception_type_id = encounter_type(HIV_RECEPTION)&.encounter_type_id
+      reception_encounter_ids = @todays_encounters
+                               .select { |encounter| encounter.encounter_type == reception_type_id }
+                               .map(&:encounter_id)
+      @observations.any? do |observation|
+        reception_encounter_ids.include?(observation.encounter_id) &&
+          observation.concept_id == concept_id('Patient present') &&
+          observation.value_coded.to_i == concept_id('Yes')
+      end
+    end
+
     def patient_not_already_staged?
       !@staged_patient_ids.include?(@patient.patient_id)
+    end
+
+    def patient_not_visiting?
+      observation = observations_for('Type of patient')
+                    .max_by { |item| [item.obs_datetime, item.obs_id] }
+      observation.blank? || observation.value_coded != concept_id('External consultation')
+    end
+
+    def patient_not_coming_for_drug_refill?
+      observation = observations_for('Type of patient')
+                    .max_by { |item| [item.obs_datetime, item.obs_id] }
+      observation.blank? || observation.value_coded != concept_id('Drug refill')
     end
 
     def patient_has_symptoms_screening?
@@ -210,10 +291,13 @@ module ArtService
       encounter_types = EncounterType.where(name: workflow_states).index_by { |type| type.name.upcase }
       encounters_by_patient_date = load_encounters(visits_by_patient.keys)
       arv_ids = Drug.arv_drugs.pluck(:drug_id)
-      concept_names = %w[AHD Symptom Yes No Fast Fast track visit Medication orders Refer to ART clinician]
-      concepts = concept_names.each_with_object({}) do |name, map|
-        map[name.downcase] = Concept.joins(:concept_names).where('concept_name.name = ?', name).first
-      end
+      concept_names = [
+        'AHD Symptom', 'Yes', 'No', 'Fast', 'Fast track visit',
+        'Medication orders', 'Refer to ART clinician', 'Patient present',
+        'Type of patient', 'External consultation', 'Drug refill',
+        'Prescribe drugs', 'Weight', 'Height (cm)'
+      ]
+      concepts = ConceptName.where(name: concept_names).index_by { |concept| concept.name.downcase }
       concept_ids = concepts.values.compact.map(&:concept_id)
       observations_by_patient = load_observations(visits_by_patient.keys, concept_ids:, arv_ids:)
       patient_states_by_patient = load_patient_states(visits_by_patient.keys)
@@ -222,10 +306,9 @@ module ArtService
       clinician_ids = User.joins(:roles).where(role: { role: 'Clinician' }).pluck(:user_id)
       activities_property = UserProperty.find_by(user_id: User.current.user_id, property: 'Activities')
       activities = OptimizedWorkflowEngine.activities(activities_property&.property_value)
-      fast_track_enabled = GlobalProperty.unscoped.find_by(property: 'enable.fast.track',
-                        location_id: User.current.location_id)&.property_value&.casecmp?('true')
-      htn_enabled = GlobalProperty.unscoped.find_by(property: 'activate.htn.enhancement',
-                        location_id: User.current.location_id)&.property_value&.casecmp?('true')
+      fast_track_enabled = global_flag('enable.fast.track')
+      htn_enabled = global_flag('activate.htn.enhancement')
+      htn_workflow = OptimizedHtnWorkflow.new if htn_enabled
       incomplete_dates = Hash.new { |dates, patient_id| dates[patient_id] = [] }
 
       ActiveRecord::Base.connection.cache do
@@ -250,7 +333,8 @@ module ArtService
               clinician_ids:,
               concepts:,
               fast_track_enabled:,
-              htn_enabled:
+              htn_enabled:,
+              htn_workflow:
             )
             next if engine.next_encounter.blank?
 
@@ -311,6 +395,11 @@ module ArtService
                                        encounter_type: encounter_type.encounter_type_id)
       scope = scope.where('encounter_datetime < ?', before) if before
       scope.distinct.pluck(:patient_id)
+    end
+
+    def global_flag(property)
+      value = GlobalProperty.unscoped.find_by(property:, location_id: User.current.location_id)&.property_value
+      value.to_s.casecmp?('true') == true
     end
   end
 end
